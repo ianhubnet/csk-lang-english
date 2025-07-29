51a16d697870e3fe439a73b394e0b8fdec8deee9
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-07-29 05:38:38 +0100

    refactor!: Merged CSK into CI3

--- language/english/upload_lang.php
+++ language/english/upload_lang.php
@@ -12,7 +12,6 @@ $lang['upload_invalid_filetype'] = 'The filetype you are attempting to upload is
 $lang['upload_invalid_filesize'] = 'The file you are attempting to upload is larger than the permitted size.';
 $lang['upload_invalid_dimensions'] = 'The image you are attempting to upload doesn\'t fit into the allowed dimensions.';
 $lang['upload_destination_error'] = 'A problem was encountered while attempting to move the uploaded file to the final destination.';
-$lang['upload_no_filepath'] = 'The upload path does not appear to be valid.';
 $lang['upload_no_file_types'] = 'You have not specified any allowed file types.';
 $lang['upload_bad_filename'] = 'The file name you submitted already exists on the server.';
 $lang['upload_not_writable'] = 'The upload destination folder does not appear to be writable.';
