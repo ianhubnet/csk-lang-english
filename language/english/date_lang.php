74e87153c59f773f851173237efe97d30e5e9c6a
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-08-12 18:11:39 +0100

    refactor: Deleted useless language files
    Also merged 'calendar_lang.php' into 'date_lang.php'.

--- language/english/date_lang.php
+++ language/english/date_lang.php
@@ -24,3 +24,53 @@ $lang['date_num_days_ago'] = '%d days ago';
 $lang['date_num_weeks_ago'] = '%d weeks ago';
 $lang['date_num_months_ago'] = '%d months ago';
 $lang['date_num_years_ago'] = '%d years ago';
+
+$lang['date_su'] = 'Su';
+$lang['date_mo'] = 'Mo';
+$lang['date_tu'] = 'Tu';
+$lang['date_we'] = 'We';
+$lang['date_th'] = 'Th';
+$lang['date_fr'] = 'Fr';
+$lang['date_sa'] = 'Sa';
+
+$lang['date_sun'] = 'Sun';
+$lang['date_mon'] = 'Mon';
+$lang['date_tue'] = 'Tue';
+$lang['date_wed'] = 'Wed';
+$lang['date_thu'] = 'Thu';
+$lang['date_fri'] = 'Fri';
+$lang['date_sat'] = 'Sat';
+
+$lang['date_sunday'] = 'Sunday';
+$lang['date_monday'] = 'Monday';
+$lang['date_tuesday'] = 'Tuesday';
+$lang['date_wednesday'] = 'Wednesday';
+$lang['date_thursday'] = 'Thursday';
+$lang['date_friday'] = 'Friday';
+$lang['date_saturday'] = 'Saturday';
+
+$lang['date_jan'] = 'Jan';
+$lang['date_feb'] = 'Feb';
+$lang['date_mar'] = 'Mar';
+$lang['date_apr'] = 'Apr';
+$lang['date_may'] = 'May';
+$lang['date_jun'] = 'Jun';
+$lang['date_jul'] = 'Jul';
+$lang['date_aug'] = 'Aug';
+$lang['date_sep'] = 'Sep';
+$lang['date_oct'] = 'Oct';
+$lang['date_nov'] = 'Nov';
+$lang['date_dec'] = 'Dec';
+
+$lang['date_january'] = 'January';
+$lang['date_february'] = 'February';
+$lang['date_march'] = 'March';
+$lang['date_april'] = 'April';
+$lang['date_mayl'] = 'May';
+$lang['date_june'] = 'June';
+$lang['date_july'] = 'July';
+$lang['date_august'] = 'August';
+$lang['date_september'] = 'September';
+$lang['date_october'] = 'October';
+$lang['date_november'] = 'November';
+$lang['date_december'] = 'December';
