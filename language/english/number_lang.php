57338e0a4f4cd1faa0a8125dda0ae4e8dd7aac75
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2025-08-18 09:16:00 +0100

    feat(number): Added extra byte units

--- language/english/number_lang.php
+++ language/english/number_lang.php
@@ -1,5 +1,11 @@
 <?php
 
+$lang['quettabyte_abbr'] = 'QB';
+$lang['ronnabyte_abbr'] = 'RB';
+$lang['yottabyte_abbr'] = 'YB';
+$lang['zettabyte_abbr'] = 'ZB';
+$lang['exabyte_abbr'] = 'EB';
+$lang['petabyte_abbr'] = 'PB';
 $lang['terabyte_abbr'] = 'TB';
 $lang['gigabyte_abbr'] = 'GB';
 $lang['megabyte_abbr'] = 'MB';
